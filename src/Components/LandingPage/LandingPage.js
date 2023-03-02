7879b92f29c5ad8e9955d0bb264aa32ad22fc993
Zachary Piontek <zakpiontek@gmail.com>
2023-03-02 13:19:19 -0800

    components folders, landingpage component and css centering started

---
+++ src/Components/LandingPage/LandingPage.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./landingpage.css";
+
+const LandingPage = () => {
+  return (
+    <div className="landingpage">
+      <h1>LANDING PAGE</h1>
+    </div>
+  );
+};
+
+export default LandingPage;
