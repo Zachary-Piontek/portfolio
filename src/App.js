bd95e6fda2353c6a2fed8e247a4beb044bb6afd7
Zachary Piontek <zakpiontek@gmail.com>
2023-03-03 11:00:47 -0800

    routes started {Contact} component route working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
 import LandingPage from "./Components/LandingPage/LandingPage";
+import { Contact } from "./Components/Contact/Contact.js";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <LandingPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
