6f5bc08e52be0930715157d741c90c5d85dbce43
Zachary Piontek <zakpiontek@gmail.com>
2023-03-03 12:01:41 -0800

    added routes to pages and css styles to <a> links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import "./App.css";
 import LandingPage from "./Components/LandingPage/LandingPage";
 import { Contact } from "./Components/Contact/Contact.js";
+import { Resume } from "./Components/Resume/Resume.js";
+import Projects from "./Components/Projects/Projects.js";
+import { About } from "./Components/About/About.js";
+
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 
 function App() {
@@ -10,6 +14,9 @@ function App() {
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </BrowserRouter>
     </div>
