b633005c66f85cfdc6ddfd66b59340206c6c04bd
Zachary Piontek <zakpiontek@gmail.com>
2023-03-02 18:53:40 -0800

    this is harder than I thought spiral effect for landing page

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -2,9 +2,31 @@ import { useState } from "react";
 import "./landingpage.css";
 
 const LandingPage = () => {
+  const [toggleOpen, setToggleOpen] = useState(false);
+  const [menuClass, setMenuClass] = useState("");
+
+  const toggleMenu = () => {
+    setToggleOpen(!toggleOpen);
+    setMenuClass(toggleOpen ? "" : "active");
+  };
+
   return (
-    <div className="landingpage">
-      <h1>LANDING PAGE</h1>
+    <div className={`menu-container ${menuClass}`}>
+      <button className="menu-toggle" onClick={toggleMenu}>
+        <h1>LD</h1>
+      </button>
+      <li style={{ "--i": 0 }} index="1">
+        <a href="/projects">Projects</a>
+      </li>
+      <li style={{ "--i": 1 }} index="2">
+        <a href="/about">About</a>
+      </li>
+      <li style={{ "--i": 2 }} index="3">
+        <a href="/contact">Contact</a>
+      </li>
+      <li style={{ "--i": 3 }} index="4">
+        <a href="/resume">Resume</a>
+      </li>
     </div>
   );
 };
