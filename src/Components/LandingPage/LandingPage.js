a0294670927cc9b409a9094a85cc2243b30d879d
Zachary Piontek <zakpiontek@gmail.com>
2023-03-04 16:31:19 -0800

    added toggle effect to button on landing page

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -4,10 +4,12 @@ import "./landingpage.css";
 const LandingPage = () => {
   const [toggleOpen, setToggleOpen] = useState(false);
   const [menuClass, setMenuClass] = useState("");
+  const [buttonText, setButtonText] = useState("Click Me");
 
   const toggleMenu = () => {
     setToggleOpen(!toggleOpen);
     setMenuClass(toggleOpen ? "" : "active");
+    setButtonText(toggleOpen ? "Click Me" : "<ZP>");
   };
 
   // in the return statement, li style is a CSS variable that is set to the index of the li element
@@ -15,7 +17,7 @@ const LandingPage = () => {
   return (
     <div className={`menu-container ${menuClass}`}>
       <button className="menu-toggle" onClick={toggleMenu}>
-        <h1>&lt;ZP&gt;</h1>
+        <h1>{buttonText}</h1>
       </button>
       <li style={{ "--i": 0 }} index="1">
         <a href="/projects" className="projects-link">
