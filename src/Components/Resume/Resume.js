6fcf354be6d3d836ed8ac9c238e95e90a1209f0a
Zachary Piontek <zakpiontek@gmail.com>
2023-03-06 18:02:42 -0800

    added resume image and display

--- src/Components/Resume/Resume.js
+++ src/Components/Resume/Resume.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./resume.css";
+import resumeImage from "./Resume-Feb20_2023.jpg";
 
 export const Resume = () => {
   return (
     <div className="resume-container">
       <h1>Resume</h1>
-      <section></section>
+      <section>
+        <img className="resume-image" src={resumeImage} alt="Resume" />
+      </section>
     </div>
   );
 };
