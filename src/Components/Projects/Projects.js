a382f5ac3d0861ee02466336ca11340e0f6af747
Zachary Piontek <zakpiontek@gmail.com>
2023-03-06 18:41:05 -0800

    setup ready for glassmorphishm effect for project cards

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -2,7 +2,31 @@ import React from "react";
 import "./projects.css";
 
 const Projects = () => {
-  return <div className="projects-container">Projects</div>;
+  return (
+    <div className="projects-container">
+      <div className="projects-box">
+        <span></span>
+        <div className="projects-content">
+          <h2>Project 1</h2>
+          <p>lorem ipsum dolor sit amet</p>
+        </div>
+      </div>
+      <div className="projects-box">
+        <span></span>
+        <div className="projects-content">
+          <h2>Project 2</h2>
+          <p>lorem ipsum dolor sit amet</p>
+        </div>
+      </div>
+      <div className="projects-box">
+        <span></span>
+        <div className="projects-content">
+          <h2>Project 3</h2>
+          <p>lorem ipsum dolor sit amet</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
