520668ab2938608603eeac235c65dbbc4aff0997
Zachary Piontek <zakpiontek@gmail.com>
2023-03-07 17:58:22 -0800

    added images/links for project cards

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,28 +1,49 @@
 import React from "react";
 import "./projects.css";
+import chromeSays from "./ChromeSays.png";
+import petStore from "./Petstore.png";
+import cryptoWarfare from "./CryptoWarfare.png";
 
 const Projects = () => {
   return (
     <div className="projects-container">
       <div className="projects-box">
-        <span></span>
         <div className="projects-content">
-          <h2>Project 1</h2>
-          <p>lorem ipsum dolor sit amet</p>
+          <h2>CryptoWarfare</h2>
+          <p>Track your future wealth</p>
+          <a
+            href="https://details.dq8jw5s2otf2n.amplifyapp.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={cryptoWarfare} alt="crypto warfare" />
+          </a>
         </div>
       </div>
       <div className="projects-box">
-        <span></span>
         <div className="projects-content">
-          <h2>Project 2</h2>
-          <p>lorem ipsum dolor sit amet</p>
+          <h2>Petstore</h2>
+          <p>Basic CRUD operations using AWS Amplify with Auth</p>
+          <a
+            href="https://main.d3pqwsrvh31zrd.amplifyapp.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={petStore} alt="pet store" />
+          </a>
         </div>
       </div>
       <div className="projects-box">
-        <span></span>
         <div className="projects-content">
-          <h2>Project 3</h2>
-          <p>lorem ipsum dolor sit amet</p>
+          <h2>Chrome Says</h2>
+          <p>A faster game than simon says</p>
+          <a
+            href="https://chrome-says.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={chromeSays} alt="chrome says" />
+          </a>
         </div>
       </div>
     </div>
